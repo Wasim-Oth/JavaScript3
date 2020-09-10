ae797d5d0394bcca9796a48d66e8042c55153585
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-09-10 17:57:21 +0200

    added index.html & script.js

---
+++ Week2/homework/pokemon-app/script.js
@@ -0,0 +1,41 @@
+function main () {
+const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
+const btn = document.querySelector('#btn')
+const select = document.querySelector('#select');
+const pokimonImg = document.querySelector('#img')
+// const error = 'somwthing went wrong';
+const log = console.log;
+
+
+btn.addEventListener('click', getPokimon);
+select.addEventListener('change', showPokimonPhoto )
+
+
+async function getPokimon(){
+    const response = await fetch (url);
+    const data =  await response.json()
+    // .catch(error => console.log(error))
+    data.results.forEach(pokimons => {
+        // log(pokimons)
+        select.innerHTML += '<option>' +  pokimons.name + '</option>'
+    })
+}
+
+async function showPokimonPhoto (){
+    const response = await fetch(url)
+    const data = await response.json()
+    // .catch(error => console.log(error))
+    data.results.forEach(pokimons =>{
+        // log(pokimons.url)
+        if (pokimons.name === select.value) {
+            fetch(pokimons.url)
+            .then(res => res.json())
+            .then (data => pokimonImg.src = data.sprites.front_shiny)
+            .catch(error => console.log(error))
+        }
+    })
+}
+
+}
+main()
+

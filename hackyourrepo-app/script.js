e8461b052e3b2b9a0c4b15013d6fd663e3c5d21d
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-09-18 11:18:49 +0200

    Update script.js

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -33,31 +33,17 @@ const section = document.querySelector('.repositories-container');
 // log(select.value);
 
 select.addEventListener('change', changeOption);
-function changeOption(){
-  if (select.value === 'SampleRepo1' ){
-    section.style.display='block';
-    repoName.textContent= placeholderRepos[0].name;
-    description.textContent= placeholderRepos[0].description;
-    forks.textContent= placeholderRepos[0].forks;
-    updated.textContent= placeholderRepos[0].updated;
+function changeOption(event){
+  for ( let i = 0; i < placeholderRepos.length; i++ ){
+    let repository= placeholderRepos[i]
+    if ( repository.name === event.target.value ){
+      section.style.display='block';
+      repoName.textContent= repository.name
+      description.textContent=repository.description
+      forks.textContent= repository.forks
+      updated.textContent= repository.updated
+   }
   }
-  else if (select.value === 'AndAnotherOne' ){
-    section.style.display='block';
-    repoName.textContent= placeholderRepos[1].name;
-    description.textContent= placeholderRepos[1].description;
-    forks.textContent= placeholderRepos[1].forks;
-    updated.textContent= placeholderRepos[1].updated;
-  }
-  if (select.value === 'HYF-Is-The-Best' ){
-    section.style.display='block';
-    repoName.textContent= placeholderRepos[2].name;
-    description.textContent= placeholderRepos[2].description;
-    forks.textContent= placeholderRepos[2].forks;
-    updated.textContent= placeholderRepos[2].updated;
-  }
-  if (select.value === '0' ){
-    section.style.display='none';
-  }
-  }
+}
 
 

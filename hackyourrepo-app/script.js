eb71adf25cf818702c7bfaa72db7e55b392a068b
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-09-27 16:55:54 +0200

    Update script.js
    added error handler for 'getData' function

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -67,10 +67,14 @@
 
       //function to add the options to select
       async function getData(){
+        try{
       let response = await fetch (url)
       let data = await response.json();
       for ( let i in data){
         select.innerHTML +=`<option> ${data[i].name} </option>`
+        }
+         } catch(error){
+             console.log(`something went wrong with getData function ${error}`)
         }
       }
   getData()

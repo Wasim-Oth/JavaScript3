576f84efc8b667af69350629dc86897ce44452cf
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-10-14 17:41:00 +0200

    Update Pagination.js

--- hackyourrepo-app/util/Pagination.js
+++ hackyourrepo-app/util/Pagination.js
@@ -1,21 +1,61 @@
 
+import {section2, pagenumbers} from "./main.js"
 
-//  export function Pagination(arr, wrapper, raw_per_page, page) {
-//     page--;
-//     let start = raw_per_page * page;
-//     let end = start + raw_per_page;
-//     let paginatedItems = arr.slice(start, end);
-//     let page_count = Math.ceil(arr.length / raw_per_page);
+export function pagination(listItem){
+    let currentPage = 1;
+    let rows = 5;
+    
+    function displayList (items, wrapper, rowsPerPage, page){
+      wrapper.innerHTML = "";
+      page--;
+      let start = rowsPerPage * page;
+      let end = start + rowsPerPage;
+      let paginatedItem = items.slice(start , end);
+      
+      for (let i = 0; i < paginatedItem.length; i++){
+        let item = paginatedItem[i];
+        let contributorsConatiner= document.createElement('div');
+        contributorsConatiner.className= 'contributors'
+        section2.appendChild(contributorsConatiner);
+        contributorsConatiner.innerHTML += 
+        `
+        <img src= ${item.avatar_url} class="img"/>
+        <a href ="https://github.com/${item.login}">${item.login}</a>
+        <div>${item.contributions}</div>
+        `
+        wrapper.appendChild(contributorsConatiner);
+      };
+    };
+
+    // function to create buttons
+    function setupPagination(items, wrapper, rowsPerPage){
+      wrapper.innerHTML = "";
+      let pageCount = Math.ceil(items.length / rowsPerPage);
+      for (let i = 1; i< pageCount + 1 ; i++){
+        let btn = paginationButton(i); 
+        wrapper.appendChild(btn);
+      }
+    }
+    
+    function paginationButton(page) {
+      let button = document.createElement('button');
+      button.innerText = page;
+
+      if (currentPage === page) 
+        button.classList.add('active');
+      
+      button.addEventListener('click', () => {
+        currentPage = page;
+        displayList(listItem, section2, rows, currentPage);
+
+        let currentBtn = document.querySelector('.pagenumbers button.active');
+        currentBtn.classList.remove('active');
+        button.classList.add('active');
+      })
+      return button;
+    }
   
-//     for (let i = 0; i < paginatedItem.length; i++) {
-//         let item = paginatedItem[i];
-//         const contributorsConatiner = document.createElement('div')
-//         contributorsConatiner.className= 'contributors'
-//         section2.appendChild(contributorsConatiner)
-
-//         contributorsConatiner.innerHTML+=
-//         `<img src= ${item.avatar_url} class="img"></img>
-//         <a href ="https://github.com/${item.login}">${item.login}</a>
-//         <div>${item.contributions}</div>`
-//     }
-// }
+    displayList(listItem,section2, rows, currentPage);
+    setupPagination(listItem, pagenumbers, rows);
+  
+  }
